5218982798e25627ac35257bacd14102b778e4f9
Martina Polášková <104367983+polaskovam@users.noreply.github.com>
2024-01-23 19:08:51 +0100

    add form and event

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -118,4 +118,9 @@ export class HousingService {
     getHousingLocationById(id: number): HousingLocation | undefined {
         return this.housingLocationList.find((housingLocation) => housingLocation.id === id)
     }
+
+
+    submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(firstName, lastName, email);
+    }
 }
